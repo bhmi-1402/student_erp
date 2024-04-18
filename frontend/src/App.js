24e2e1400c203fe80e93dd0f856c087395922bd4
bhoomi <bhoomi1414agarwal@gmail.com>
2024-04-18 17:47:08 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,31 +3,17 @@ import './App.css';
 import { useSelector , useDispatch} from 'react-redux'; 
 import { addUser } from './store/userSlice';
 import { useEffect, useState } from 'react';
+
+import NavBar from './components/common/Navbar';
+import Strip from './components/HomePage/Strip';
 function App() {
   
-  const [coin , setCoin] = useState(1);
-  const [reset,SetReset] = useState(true);
-  function increseHandler(){
-    setCoin(coin+1)
-    
-  }
   
-  function setValue(){
-    setCoin(1000)
-  }
-
-  useEffect(()=>{
-    setValue();
-  },[coin]);
-
-  console.log("Render");
 
   return (
     <div className="App">
 
-      <h1>My balance is  : {coin} rupees</h1>
-      <button onClick={increseHandler}>Increase</button>
-      <button onClick={()=>SetReset(!reset)}>Reset</button>
+      
       <NavBar></NavBar>
       <Strip></Strip>
 
