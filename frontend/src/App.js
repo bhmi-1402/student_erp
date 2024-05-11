446ad6cd2baad1cb0ce5768b2429289aa9513f28
Naveen Chaudhary <98143534+VENOMnvn@users.noreply.github.com>
2024-05-11 19:53:40 +0530

    Admin.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,11 +15,12 @@ import StudentComplain from './pages/student/StudentComplain.js';
 import StudentProfile from './pages/student/StudentProfile';
 import StudentSideBar from './pages/student/StudentSideBar';
 import StudentSubjects from './pages/student/StudentSubjects';
-import TeacherClassDetails from './pages/teacher/TeacherClassDetail.js';
-import TeacherProfile from './pages/teacher/TeacherProfile.js';
-import TeacherSideBar from './pages/teacher/TeacherSideBar.js';
-import TeacherViewStudent from './pages/teacher/TeacherViewStudent.js';
+// import TeacherClassDetails from './pages/teacher/TeacherClassDetail.js';
+// import TeacherProfile from './pages/teacher/TeacherProfile.js';
+// import TeacherSideBar from './pages/teacher/TeacherSideBar.js';
+// import TeacherViewStudent from './pages/teacher/TeacherViewStudent.js';
 import path from './path.js';
+import AdminForm from './pages/AdminForm.js';
 
 function App() {
 
@@ -36,10 +37,11 @@ function App() {
         <Route path='/Studentprofile' element={<StudentProfile/>}></Route>
         <Route path='/StudentComplain' element={<StudentComplain/>}></Route>
         <Route path='/StudentAttendance' element={<StudentAttendance/>}></Route>
-        <Route path='/TeacherClassDetail' element={<TeacherClassDetails/>}></Route>
+        <Route path='/adminform' element={<AdminForm></AdminForm>}></Route>
+        {/* <Route path='/TeacherClassDetail' element={<TeacherClassDetails/>}></Route>
         <Route path='/TeacherViewStudent' element={<TeacherViewStudent/>}></Route>
         <Route path='/TeacherSideBar' element={<TeacherSideBar/>}></Route>
-        <Route path='/TeacherProfile' element={<TeacherProfile/>}></Route>
+        <Route path='/TeacherProfile' element={<TeacherProfile/>}></Route> */}
         <Route path='/Logout' element={<Logout/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
       </Routes>
@@ -47,14 +49,15 @@ function App() {
       <div>
         <a href={'/login'} className='mx-2'>Login</a>
         <a href={'/logout'} className='mx-2'>Logout</a>
-        <a href={'/TeacherProfile'} className='mx-2'>TeacherProfile</a>
-        <a href={'/TeacherSideBar'} className='mx-2'>TeacherSideBar</a>
-        <a href={'/TeacherViewStudent'} className='mx-2'>TeacherViewStudent</a>
-        <a href={'/TeacherClassDetail'} className='mx-2'>TeacherClassDetail</a>
-        <a href={'/TeacherAttendance'} className='mx-2'>TeacherAttendance</a>
+        {/* <a href={'/TeacherProfile'} className='mx-2'>TeacherProfile</a> */}
+        {/* <a href={'/TeacherSideBar'} className='mx-2'>TeacherSideBar</a> */}
+        {/* <a href={'/TeacherViewStudent'} className='mx-2'>TeacherViewStudent</a> */}
+        {/* <a href={'/TeacherClassDetail'} className='mx-2'>TeacherClassDetail</a> */}
+        {/* <a href={'/TeacherAttendance'} className='mx-2'>TeacherAttendance</a> */}
         <a href={'/StudentAttendance'} className='mx-2'>StudentAttendance</a>
         <a href={'/StudentAttendance'} className='mx-2'>StudentAttendance</a>
         <a href={'/StudentComplain'} className='mx-2'>StudentComplain</a>
+        <a href={'/adminform'} className='mx-2'>StudentComplain</a>
         <a href={'/chooseUser'}>chooseUser</a>
 
 
