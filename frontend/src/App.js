94326e8e46c36d434c3b93d516ae296523f37a10
Naveen Chaudhary <98143534+VENOMnvn@users.noreply.github.com>
2024-05-11 22:46:06 +0530

    ADMIN FORM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,41 +11,32 @@ import Login from "./pages/Login.js";
 import Logout from "./pages/Logout.js";
 // import Present from './pages/Present.js';
 import StudentAttendance from "./pages/student/StudentAttendence.js";
-
 import StudentProfile from "./pages/student/StudentProfile";
 import StudentSideBar from "./pages/student/StudentSideBar";
 import StudentSubjects from "./pages/student/StudentSubjects";
-
 import TeacherProfile from "./pages/teacher/TeacherProfile.js";
 import TeacherSideBar from "./pages/teacher/TeacherSideBar.js";
 import path from "./path.js";
+import AdminForm from "./pages/AdminForm.js";
 
 function App() {
   return (
     <div className="App">
-     
+
       <NavBar></NavBar>
       <Routes>
         {/* <Route path='/present' element={<Present/>}></Route> */}
         <Route path="/home" element={<Home/>}></Route>
         <Route path="/chooseUser" element={<ChooseUser />}></Route>
-        <Route path="/StudentSidebar" element={<StudentSideBar />}>
-         
-        </Route>
-        <Route path="/StudentSubjects" element={<StudentSubjects />}>
-        
-        </Route>
+        <Route path="/StudentSidebar" element={<StudentSideBar />}></Route>
+        <Route path="/StudentSubjects" element={<StudentSubjects />}></Route>
         <Route path="/StudentProfile" element={<StudentProfile />}></Route>
-
-        <Route
-          path="/StudentAttendance"
-          element={<StudentAttendance />}
-        ></Route>
-
+        <Route path="/StudentAttendance" element={<StudentAttendance />}></Route>
         <Route path="/TeacherSideBar" element={<TeacherSideBar />}></Route>
         <Route path="/TeacherProfile" element={<TeacherProfile />}></Route>
         <Route path="/Logout" element={<Logout />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/adminform" element={<AdminForm></AdminForm>}></Route>
       </Routes>
 
       <div>
@@ -64,12 +55,9 @@ function App() {
         <a href={"/TeacherViewStudent"} className="mx-2">
           TeacherViewStudent
         </a>
-        
-        
         <a href={"/StudentAttendance"} className="mx-2">
           StudentAttendance
         </a>
-        
         <a href={"/chooseUser"}>chooseUser</a>
       </div>
     </div>
--- frontend/src/pages/AdminForm.js
+++ frontend/src/pages/AdminForm.js
@@ -2,109 +2,113 @@ import { useState } from "react";
 import path from "../path";
 import axios from "axios";
 import { Password } from "@mui/icons-material";
-
+import { Button, TextField,Select,MenuItem, FormControl, InputLabel, LinearProgress} from "@mui/material";
+import './AdminForm.css';
 
 const AdminForm = ()=>{
-
+    const [load,setLoad] = useState(false);
     const [data,setData] = useState({
-    name : "",
-    email : "",
-    password : "",
-    phoneNumber : "",
-    class : "",
-    rollNumber : "",
-    subject1:"",
-    subject2:"",
-    subject3:"",
-    subject4:"",
-    subject5:""
-});
+        name:"",
+        email:"",
+        password:"",
+        rollNumber:"",
+        class:"",
+        currentSemester:"",
+        gender:"",
+        subject1:"",
+        subject2:"",
+        subject3:"",
+        subject4:"",
+        subject5:""
+    });
 
-    const addToTeacher = async ()=>{
+    const handleSubmit = async ()=>{
+        setLoad(true);
         try{
-            const res = await axios.post(path+'addTeachers',data);
-            console.log(res);
+            const res = await axios.post(path+'admin/addToStudents',data);
+            if(res.data.error){
+                alert("Some Error Occured At Backend");
+            }
         }catch(err){
-            console.log(err);
+            alert("Some Error Occured");
         }
+        setLoad(false);
     }
-    const addToStudent = async ()=>{
-        try{
-            const res = await axios.post(path+'addStudents',data);
-            console.log(res);
-        }catch(err){
-            console.log(err);
-        }
-    }
-
 return <>
-<h1 className="mx-auto text-large">Please Enter Details of the new User</h1>
-<form class="max-w-md mx-auto">
+<div className="adminform-title">Add Student Data to Database</div>
+{
+    load && <LinearProgress></LinearProgress>
+}
+<div className="adminform">
+<TextField id="outlined-basic" label="Name" variant="outlined" value={data.name} onChange={e=>setData({...data,name:e.target.value.toLowerCase()})}/>
+<TextField id="outlined-basic" label="Roll Number" variant="outlined" value={data.rollNumber} onChange={e=>setData({...data,rollNumber:e.target.value.toLowerCase()})} />
+<TextField id="outlined-basic" label="Email" variant="outlined" value={data.email} onChange={e=>setData({...data,email:e.target.value.toLowerCase()})}/>
+<TextField id="outlined-basic" label="Password" variant="outlined" value={data.password} onChange={e=>setData({...data,password:e.target.value})}/>
+{/* <TextField id="outlined-basic" label="Class" variant="outlined" value={data.class} onChange={e=>setData({...data,class:e.target.value.toLowerCase()})}/> */}
+<FormControl>
+<InputLabel>Class</InputLabel>
+<Select
+    label="Branch"
+    value={data.class} onChange={e=>setData({...data,class:e.target.value.toLowerCase()})}
+    >
+    <MenuItem value={"cs"}>Computer Science</MenuItem>
+    <MenuItem value={"it"}>Information Technology</MenuItem>
+    <MenuItem value={"ece"}>Electronics</MenuItem>
+    <MenuItem value={"ee"}>Electrical</MenuItem>
+    <MenuItem value={"eee"}>Electrical & Electronics</MenuItem>
+    <MenuItem value={"civil"}>Civil</MenuItem>
+    <MenuItem value={"me"}>Mechanical</MenuItem>
   
-  <div class="relative z-0 w-full mb-5 group">
-      <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required  value={data.email} onChange={e=>setData({...data,email:e.target.value})}/>
-      <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
-  </div>
+  </Select>
+  </FormControl>
+{/* <TextField id="outlined-basic" label="Current Semester" variant="outlined" /> */}
+<FormControl>
+<InputLabel>Current Semester</InputLabel>
+<Select
+    label="Current Semester"
+    value={data.currentSemester} 
+    onChange={e=>setData({...data,currentSemester:e.target.value})}
+    >
+    <MenuItem value={"1"}>First</MenuItem>
+    <MenuItem value={"2"}>Second</MenuItem>
+    <MenuItem value={"3"}>Third</MenuItem>
+    <MenuItem value={"4"}>Fourth</MenuItem>
+    <MenuItem value={"5"}>Fifth</MenuItem>
+    <MenuItem value={"6"}>Sixth</MenuItem>
+    <MenuItem value={"7"}>Seventh</MenuItem>
+    <MenuItem value={"8"}>Eight</MenuItem>
+  </Select>
+  </FormControl>
 
-  <div class="relative z-0 w-full mb-5 group">
-      <input type="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value={data.password} onChange={e=>setData({...data,password:e.target.value})}/>
-      <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
-  </div>
-
-
-  <div class="grid md:grid-cols-2 md:gap-6">
-    <div class="relative z-0 w-full mb-5 group">
-        <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value={data.name} onChange={e=>setData({...data,name:e.target.value})}/>
-        <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
-    </div>
-    <div class="relative z-0 w-full mb-5 group">
-        <input type="text" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={data.rollNumber} onChange={e=>setData({...data,rollNumber:e.target.value})}/>
-        <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Roll Number</label>
-    </div>
-  </div>
-  
-  <div class="grid md:grid-cols-2 md:gap-6">
-    <div class="relative z-0 w-full mb-5 group">
-        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={data.phoneNumber} onChange={e=>setData({...data,phoneNumber:e.target.value})} />
-        <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number</label>
-    </div>
-    <div class="relative z-0 w-full mb-5 group">
-        <input type="text" name="floating_company" id="floating_company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={data.class} onChange={e=>setData({...data,calss:e.target.value})} />
-        <label for="Class" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Class</label>
-    </div>
-  </div>
-
-  <hr className="mt-6">
-  </hr>
-  <p className="text-gray-500">In Case of Students Only </p>
- 
-  <div className="my-2 flex justify-center flex-col">
-
-    <div class="relative z-0 w-full mb-5 group">
-        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={data.subject1} onChange={e=>setData({...data,subject1:e.target.value})}/>
-        <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Subject 1</label>
-    </div>
-    <div class="relative z-0 w-full mb-5 group">
-        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={data.subject2} onChange={e=>setData({...data,subject2:e.target.value})} />
-        <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Subject 2</label>
-    </div>
-    <div class="relative z-0 w-full mb-5 group">
-        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={data.subject3} onChange={e=>setData({...data,subject3:e.target.value})} />
-        <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Subject 3</label>
-    </div>
-    <div class="relative z-0 w-full mb-5 group">
-        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={data.subject4} onChange={e=>setData({...data,subject4:e.target.value})}/>
-        <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Subject 4</label>
-    </div>
-    <div class="relative z-0 w-full mb-5 group">
-        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={data.subject5} onChange={e=>setData({...data,subject5:e.target.value})} />
-        <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Subject 5</label>
-    </div>
-  </div>
-
-  <button  class="text-white bg-blue-700 mx-2 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={addToTeacher}>Add to Teachers</button>
-  <button  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={addToStudent}>Add to Students</button>
-</form>
+{/* <TextField id="outlined-basic" label="Gender" variant="outlined" value={data.gender} onChange={e=>setData({...data,gender:e.target.value})}/> */}
+<FormControl>
+<InputLabel>Gender</InputLabel>
+<Select
+    value={data.gender}
+    label="Gender"
+    placeholder="Gender"
+    onChange={e=>setData({...data,gender:e.target.value})}
+    >
+    <MenuItem value={"male"}>Male</MenuItem>
+    <MenuItem value={"female"}>Female</MenuItem>
+    <MenuItem value={"other"}>Other</MenuItem>
+  </Select>
+</FormControl>
+<div></div>
+<div>Academics Details</div>
+<div></div>
+<hr></hr>
+<hr></hr>
+<TextField id="outlined-basic" label="Subject 1" variant="outlined" value={data.subject1} onChange={e=>setData({...data,subject1:e.target.value.toLowerCase()})}/>
+<TextField id="outlined-basic" label="Subject 2" variant="outlined" value={data.subject2} onChange={e=>setData({...data,subject2:e.target.value.toLowerCase()})}/>
+<TextField id="outlined-basic" label="Subject 3" variant="outlined" value={data.subject3} onChange={e=>setData({...data,subject3:e.target.value.toLowerCase()})}/>
+<TextField id="outlined-basic" label="Subject 4" variant="outlined" value={data.subject4} onChange={e=>setData({...data,subject4:e.target.value.toLowerCase()})}/>
+<TextField id="outlined-basic" label="Subject 5" variant="outlined" value={data.subject5} onChange={e=>setData({...data,subject5:e.target.value.toLowerCase()})}/>
+<div></div>
+<div>
+<Button variant="contained" disabled={load} onClick={handleSubmit}>Add Student</Button>
+</div>
+</div>
 </>;
 }
 export default AdminForm;
