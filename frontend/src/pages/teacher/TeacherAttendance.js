daaf6ec394f16c9b2cda723750d61b6b16e689bc
Naveen Chaudhary <98143534+VENOMnvn@users.noreply.github.com>
2024-05-12 20:17:09 +0530

    Teacher Attedence

--- frontend/src/pages/teacher/TeacherAttendance.js
+++ frontend/src/pages/teacher/TeacherAttendance.js
@@ -7,6 +7,7 @@ import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import { useState } from "react";
+import { Button } from "@mui/material";
 
 const data = [
   { name: "Abhay", _id: "1" },
@@ -24,21 +25,52 @@ const TeacherAttendance = () => {
   const [students, setStudents] = useState(data);
   const [present, setPresent] = useState([]);
 
-  const handleStudents = (event, newStudents) => {
-    setPresent(newStudents);
-  };
+  const handlePresent = (id)=>{
+      const presentArray = [...present,id];
+      setPresent(presentArray);
+  }
+  const handleAbsent = (id)=>{
+    // 2
+    let presentArray = present;
+    // 1 2 3 4 5 
 
-  console.log(students);
 
+    const TempArray = presentArray.filter((studentID) => {
+      return studentID != id 
+      // 2 != 2
+    })
+    setPresent(TempArray);
+  }
+
+  const handleSubmit = ()=>{
+        console.log(present);
+  }
+  const isPresent = (id)=>{
+        return present.includes(id);   
+  }
+  
   return (
     <>
       <div>Mark Attendence</div>
       <div>
-          {
-            students.map(student=><div>
+        {students.map((student) => (
+          <div className="attendence-name">
+            <p>{student._id}</p>
+            <div>
+              <p>{student.name}</p>
+            </div>
+            <div>
               
-            </div>)
-          }
+              {
+                isPresent(student._id) ? <Button variant="contained" sx={{backgroundColor:"green"}} onClick={()=>handleAbsent(student._id)}> Present </Button> : <Button variant="outlined" sx={{color:"red",borderColor:"red"}} onClick={ ()=>handlePresent(student._id)} > Absent </Button>
+              }
+
+            </div>
+          </div>
+        ))}
+        <div className="flex justify-center">
+        <Button variant="contained" onClick={handleSubmit}>Submit Attendence</Button>
+        </div>
       </div>
     </>
   );
